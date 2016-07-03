29f09b7ae4c674e27d61a33c91c8cc45db5db3bc
andreas <andreas.luethi@gmx.net>
2016-07-03 23:15:19 +0200

    Initial commit

---
+++ Test/src/ch/luethi/ringbuffer/RingBufferTest.java
@@ -0,0 +1,295 @@
+package ch.luethi.ringbuffer;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.rules.TestName;
+
+import java.io.File;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+/**
+ * Created by luethiand on 21.06.2016.
+ */
+public class RingBufferTest {
+
+    @Rule
+    public TestName testName = new TestName();
+
+    @Before
+    public void setUp() throws Exception {
+        deleteDtataFile();
+    }
+
+    private void deleteDtataFile() {
+        File file = new File(RingBuffer.TEST_DAT_FILE);
+        if (file.delete()) {
+            System.out.println(testName.getMethodName() + ", file '" + file.getName() + "' is deleted!");
+        }
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+
+    @Test
+    public void openExistingStorageTest() {
+        RingBuffer rb = new RingBuffer(234);
+        assertEquals("Capacity not correct", 234, rb.getCapacity());
+        assertEquals("Count not correct", 0, rb.getCount());
+        assertEquals("Last not correct", 0, rb.getLast());
+
+        rb = new RingBuffer();
+        assertEquals("Capacity not correct", 234, rb.getCapacity());
+        assertEquals("Count not correct", 0, rb.getCount());
+        assertEquals("Last not correct", 0, rb.getLast());
+    }
+
+    @Test
+    public void reopenStorageTest() {
+        RingBuffer rb = new RingBuffer(1234);
+        assertEquals("Capacity not correct", 1234, rb.getCapacity());
+        assertEquals("Count not correct", 0, rb.getCount());
+        assertEquals("Last not correct", 0, rb.getLast());
+        rb.close();
+
+        rb = new RingBuffer();
+        assertEquals("Capacity not correct", 1234, rb.getCapacity());
+        assertEquals("Count not correct", 0, rb.getCount());
+        assertEquals("Last not correct", 0, rb.getLast());
+    }
+
+    @Test
+    public void initialOpenStorageTest() {
+        RingBuffer rb = new RingBuffer(200);
+        rb.close();
+        deleteDtataFile();
+        rb = new RingBuffer();
+        assertEquals("Capacity not correct", 0, rb.getCapacity());
+        assertEquals("Count not correct", 0, rb.getCount());
+        assertEquals("Last not correct", 0, rb.getLast());
+    }
+
+    @Test
+    public void setCapacityTest() {
+        RingBuffer rb = new RingBuffer();
+        rb.setCapacity(4711);
+        assertEquals("Capacity not correct", 4711, rb.getCapacity());
+        assertEquals("Count not correct", 0, rb.getCount());
+        assertEquals("Last not correct", 0, rb.getLast());
+    }
+
+    @Test
+    public void setRecLenTest() {
+        RingBuffer rb = new RingBuffer();
+        rb.setCapacity(20);
+        rb.setRecLen((byte) 123);
+        assertEquals("Capacity not correct", 20, rb.getCapacity());
+        assertEquals("Reclen not correct", 123, rb.getRecLen());
+        assertEquals("Count not correct", 0, rb.getCount());
+        assertEquals("Last not correct", 0, rb.getLast());
+        byte[] ba = new byte[123];
+        for (byte i = 0; i < ba.length; i++) {
+            ba[i] = (byte) (i%50);
+        }
+        rb.push(ba);
+        byte[] ba2 = rb.pop();
+        assertEquals("Record size not correct", ba.length, ba2.length);
+        assertArrayEquals("Record content not correct", ba, ba2);
+    }
+
+
+    @Test
+    public void pushWith_0_CapacityTest() {
+        RingBuffer rb = new RingBuffer();
+        assertEquals("Capacity not correct", 0, rb.getCapacity());
+        byte[] ba = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+        thrown.expect(RuntimeException.class);
+        thrown.expectMessage("Storage capacity is 0");
+        rb.push(ba);
+    }
+
+
+    @Test
+    public void pushPopTest() {
+        RingBuffer rb = new RingBuffer(100);
+        byte[] ba = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+        long last = rb.getLast();
+        rb.push(ba);
+        assertEquals("Count not correct", 1, rb.getCount());
+        assertEquals("Last not correct", last + 1, rb.getLast());
+
+        rb = new RingBuffer();
+        byte[] ba2 = rb.pop();
+        assertEquals("Record size not correct", ba.length, ba2.length);
+        assertArrayEquals("Record content not correct", ba, ba2);
+
+        byte[] ba3 = {11, 12, 13, 14, 15, 16, 17, 18, 19, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120};
+        rb.push(ba3);
+        assertEquals("Count not correct", 1, rb.getCount());
+        assertEquals("Last not correct", last + 1, rb.getLast());
+
+        rb.close();
+        rb = new RingBuffer();
+
+        ba2 = rb.pop();
+        assertEquals("Record size not correct", ba2.length, ba3.length);
+        assertArrayEquals("Record content not correct", ba2, ba3);
+    }
+
+
+    @Test
+    public void pushPeekDeleteTest() {
+        RingBuffer rb = new RingBuffer(200);
+        byte[] ba = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+        rb.push(ba);
+        long lastcnt = rb.getCount();
+        long last = rb.getCount();
+        rb.close();
+
+        rb = new RingBuffer();
+        byte[] ba2 = rb.peek();
+        assertEquals("Count not correct", lastcnt, rb.getCount());
+        assertEquals("Last not correct", last, rb.getLast());
+
+        assertEquals("Record size not correct", ba.length, ba2.length);
+        assertArrayEquals("Record content not correct", ba, ba2);
+
+        rb.delete();
+        assertEquals("Count not correct", lastcnt - 1, rb.getCount());
+        assertEquals("Last not correct", last - 1, rb.getLast());
+    }
+
+
+    @Test
+    public void pushPeekDeleteMultipleTest() {
+        RingBuffer rb = new RingBuffer(50);
+        byte[] ba = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+        for (byte i = 0; i < 10; i++) {
+            ba[0] = i;
+            rb.push(ba);
+        }
+        long lastcnt = rb.getCount();
+        long last = rb.getCount();
+        rb.close();
+
+        rb = new RingBuffer();
+        byte[][] baa2 = rb.peek((byte) 5);
+        assertEquals("Wrong amount peeked", 5, baa2.length);
+
+        assertEquals("Count not correct", lastcnt, rb.getCount());
+        assertEquals("Last not correct", last, rb.getLast());
+
+        for (byte i = 0; i < 5; i++) {
+            assertEquals("Record size not correct", ba.length, baa2[i].length);
+            assertEquals("Record content not correct", 9 - i, baa2[i][0]);
+        }
+
+        rb.close();
+        rb = new RingBuffer();
+        baa2 = rb.peek(4);
+        assertEquals("Wrong amount peeked", 4, baa2.length);
+
+        assertEquals("Count not correct", lastcnt, rb.getCount());
+        assertEquals("Last not correct", last, rb.getLast());
+
+        for (byte i = 0; i < 4; i++) {
+            assertEquals("Record size not correct", ba.length, baa2[i].length);
+            assertEquals("Record content not correct", 9 - i, baa2[i][0]);
+        }
+
+        baa2 = rb.peek(20);
+        assertEquals("Wrong amount peeked", 10, baa2.length);
+
+        assertEquals("Count not correct", lastcnt, rb.getCount());
+        assertEquals("Last not correct", last, rb.getLast());
+
+        for (byte i = 0; i < 10; i++) {
+            assertEquals("Record size not correct", ba.length, baa2[i].length);
+            assertEquals("Record content not correct", 9 - i, baa2[i][0]);
+        }
+
+    }
+
+    @Test
+    public void wrongRecordSizeTest() {
+        RingBuffer rb = new RingBuffer(100);
+        byte[] ba = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21};
+        thrown.expect(RuntimeException.class);
+        thrown.expectMessage("Date length error, length must be 20");
+        assertEquals("Count not correct", 0, rb.getCount());
+        rb.push(ba);
+    }
+
+    @Test
+    public void wrapTest() {
+        RingBuffer rb = new RingBuffer(100);
+        byte[] ba = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+        for (byte i = 0; i < 124; i++) {
+            ba[0] = i;
+            rb.push(ba);
+        }
+        assertEquals("Count not correct", 100, rb.getCount());
+        assertEquals("Last not correct", 24, rb.getLast());
+
+        byte c = 123;
+        for (byte i = 0; i < 58; i++) {
+            ba = rb.pop();
+            assertEquals("Last not correct", c, ba[0]);
+            c--;
+        }
+        assertEquals("Count not correct", 42, rb.getCount());
+        assertEquals("Last not correct", 66, rb.getLast());
+    }
+
+    @Test
+    public void underflowTest() {
+        RingBuffer rb = new RingBuffer(10);
+        byte[] ba = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+        for (byte i = 0; i < rb.getCapacity(); i++) {
+            ba[0] = i;
+            rb.push(ba);
+        }
+        assertEquals("Count not correct", 10, rb.getCount());
+
+        byte c = 9;
+        for (byte i = 0; i < rb.getCapacity(); i++) {
+            ba = rb.pop();
+            assertEquals("Last not correct", c, ba[0]);
+            c--;
+        }
+        assertEquals("Count not correct", 0, rb.getCount());
+        ba = rb.pop();
+        assertNull("Null reference expected", ba);
+        assertEquals("Count not correct", 0, rb.getCount());
+        assertEquals("Last not correct", 0, rb.getLast());
+        byte[] ba2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 88, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+        rb.push(ba2);
+        assertEquals("Count not correct", 1, rb.getCount());
+        assertEquals("Last not correct", 1, rb.getLast());
+        assertEquals("Last not correct", 88, ba2[9]);
+    }
+
+    @Test
+    public void push_1_000_000_Test() {
+        RingBuffer rb = new RingBuffer(1000000);
+        byte[] ba = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+        for (long i = 0; i < rb.getCapacity(); i++) {
+            ba[0] = (byte) (i);
+            rb.push(ba);
+        }
+        assertEquals("Count not correct", rb.getCapacity(), rb.getCount());
+
+    }
+
+
+}
---
+++ src/ch/luethi/ringbuffer/RingBuffer.java
@@ -0,0 +1,218 @@
+/*
+ * RingBuffer is ... --todo.
+ * Copyright (C) 2016  Andreas Lüthi
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package ch.luethi.ringbuffer;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
+
+public class RingBuffer {
+
+    public static final String TEST_DAT_FILE = "test.dat";
+    private long capacity;
+    private long count;
+    private long last;
+
+    private byte recLen = 20;
+    private static final byte headerLen = 16;
+    private RandomAccessFile raf;
+
+
+    public RingBuffer(long initCapacity) {
+        capacity = initCapacity;
+        try {
+            raf = new RandomAccessFile(TEST_DAT_FILE, "rw");
+            setCapacity(capacity);
+            count = 0;
+            last = 0;
+            updateHeader();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public RingBuffer() {
+        try {
+            boolean rafExits = (new File(TEST_DAT_FILE)).exists();
+            raf = new RandomAccessFile(TEST_DAT_FILE, "rw");
+            capacity = (raf.length() - headerLen) / recLen;
+            if (rafExits) {
+                readHeader();
+            } else {
+                count = 0;
+                last = 0;
+                updateHeader();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void close() {
+        try {
+            raf.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void setCapacity(long capacity) {
+        try {
+            this.capacity = capacity;
+            raf.setLength((capacity * recLen) + headerLen);
+            updateHeader();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public byte getRecLen() {
+        return recLen;
+    }
+
+    public void setRecLen(byte recLen) {
+        this.recLen = recLen;
+    }
+
+    public void push(byte[] data) {
+        if (data.length != recLen) {
+            throw new RuntimeException("Date length error, length must be " + recLen);
+        }
+        if (this.capacity == 0) {
+            throw new RuntimeException("Storage capacity is 0");
+        }
+        try {
+            count = Math.min(count + 1, capacity);
+            last = (last + 1) % capacity;
+            raf.seek(headerLen + (last * recLen));
+            raf.write(data);
+            updateHeader();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public byte[] pop() {
+        if (count > 0) {
+            try {
+                raf.seek(headerLen + (last * recLen));
+                byte[] ba = new byte[recLen];
+                raf.read(ba);
+                count = count - 1;
+                last = (last == 0 ? capacity : last) - 1;
+                updateHeader();
+                return ba;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    public byte[] peek() {
+        if (count > 0) {
+            try {
+                raf.seek(headerLen + (last * recLen));
+                byte[] ba = new byte[recLen];
+                raf.read(ba);
+                return ba;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    public byte[][] peek(int num) {
+        ArrayList<byte[]> list = new ArrayList<byte[]>(num);
+        long tlast = last;
+        long mnum = Math.min(count, num);
+
+        // public static byte[] copyOfRange(byte[] original, int from, int to)
+
+        for (int i = 0; i < mnum; i++) {
+            try {
+                raf.seek(headerLen + (tlast * recLen));
+                byte[] ba = new byte[recLen];
+                raf.read(ba);
+                list.add(ba);
+                tlast = (tlast == 0 ? capacity : tlast) - 1;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        byte[][] ret = new byte[list.size()][recLen];
+        return list.toArray(ret);
+    }
+
+
+    public void delete() {
+        if (count > 0) {
+            count = count - 1;
+            last = (last == 0 ? capacity : last) - 1;
+            updateHeader();
+        }
+    }
+
+    public void delete(int num) {
+        if (count > 0) {
+            long mnum = Math.min(count, num);
+            count = count - mnum;
+            last = (last < mnum ? capacity : last) - mnum; // // TODO: -- wrong !
+            updateHeader();
+        }
+    }
+
+
+    public long getCount() {
+        return count;
+    }
+
+    public long getCapacity() {
+        return capacity;
+    }
+
+    public long getLast() {
+        return last;
+    }
+
+    private void updateHeader() {
+        try {
+            raf.seek(0);
+            raf.writeLong(count);
+            raf.writeLong(last);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void readHeader() throws IOException {
+        raf.seek(0);
+        count = raf.readLong();
+        last = raf.readLong();
+
+    }
+
+}
